f1e3d6ea6c96b64acd4ca079df904fef7d087a79
Gabriel Watkinson <gabriel.watkinson@ensae.fr>
2021-04-15 20:27:54 +0200

    Add automatic image builder

---
+++ images.py
@@ -0,0 +1,13 @@
+import os
+
+image_dir = "output/images/"
+images = os.listdir(image_dir)
+imgs = [image_dir + img for img in images]
+sorted_images = sorted(imgs, key=lambda t: os.stat(t).st_mtime)
+md = "images.md"
+
+with open(md, "w") as f:
+    f.write("# Images")
+    for img in sorted_images:
+        f.write(f"\n\n## {img}")
+        f.write(f"\n\n![{img}](./{img})")
